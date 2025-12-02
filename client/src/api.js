b39832eec20828085551c8810675518483cc9b7f
akhilw0811 <149025093+akhilw0811@users.noreply.github.com>
2025-12-02 14:46:05 -0500

    Delete client directory

--- client/src/api.js
+++
@@ -1,8 +0,0 @@
-import axios from "axios";
-export const api = axios.create({ baseURL: "http://localhost:4000/api" });
-export function setToken(token) {
-  if (token) api.defaults.headers.common["Authorization"] = `Bearer ${token}`;
-  else delete api.defaults.headers.common["Authorization"];
-}
-
-
